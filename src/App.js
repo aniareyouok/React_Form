03591fa1e485c9c1518b16b05645d43241c79987
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-11-21 00:39:22 +0100

    input gets print to console with useState

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import MyForm from './components/MyForm'
 
 function App() {
 
-
-
     return (
         <Fragment>
             <MyForm
@@ -28,50 +26,50 @@ function App() {
 
 export default App;
 
-            {/*<wrapper>*/}
-            {/*    <h1>React Formulier</h1>*/}
-
-            {/*    <form id="review" onSubmit={handleSubmit}>*/}
-            {/*        <fieldset>*/}
-            {/*            <legend> Gegevens</legend>*/}
-
-            {/*            <label htmlFor="name_input">Naam:</label>*/}
-            {/*            <input*/}
-            {/*                type="text"*/}
-            {/*                id="name_input"*/}
-            {/*            />*/}
-
-            {/*            <label htmlFor="age_input">Leeftijd:</label>*/}
-            {/*            <input type="number" id="age_input"/>*/}
-
-            {/*        </fieldset>*/}
-
-            {/*        <fieldset>*/}
-            {/*            <legend>Jouw review</legend>*/}
-
-            {/*            <label htmlFor="review_message">Opmerkingen:</label>*/}
-            {/*            <textarea*/}
-            {/*                id="review_message"*/}
-            {/*                placeholder="Wat vond je van het recept?"*/}
-            {/*            />*/}
-
-            {/*            <span>*/}
-            {/*                <input*/}
-            {/*                    type="checkbox"*/}
-            {/*                    id="subscribe"*/}
-            {/*                />*/}
-            {/*                Ik schrijf me in voor de nieuwsbrief*/}
-            {/*            </span>*/}
-
-            {/*            <button*/}
-            {/*                type="submit"*/}
-            {/*                id="submit_button"*/}
-            {/*            >*/}
-            {/*                Versturen*/}
-            {/*            </button>*/}
-            {/*        </fieldset>*/}
-
-            {/*    </form>*/}
-            {/*</wrapper>*/}
+            // /*<wrapper>*/
+            // /*    <h1>React Formulier</h1>*/
+            //
+            // /*    <form id="review" onSubmit={handleSubmit}>*/
+            // /*        <fieldset>*/
+            // {/*            <legend> Gegevens</legend>*/}
+            //
+            // {/*            <label htmlFor="name_input">Naam:</label>*/}
+            // {/*            <input*/}
+            // {/*                type="text"*/}
+            // {/*                id="name_input"*/}
+            // {/*            />*/}
+            //
+            // {/*            <label htmlFor="age_input">Leeftijd:</label>*/}
+            // {/*            <input type="number" id="age_input"/>*/}
+            //
+            // {/*        </fieldset>*/}
+            //
+            // {/*        <fieldset>*/}
+            // {/*            <legend>Jouw review</legend>*/}
+            //
+            // {/*            <label htmlFor="review_message">Opmerkingen:</label>*/}
+            // {/*            <textarea*/}
+            // {/*                id="review_message"*/}
+            // {/*                placeholder="Wat vond je van het recept?"*/}
+            // {/*            />*/}
+            //
+            // {/*            <span>*/}
+            // {/*                <input*/}
+            // {/*                    type="checkbox"*/}
+            // {/*                    id="subscribe"*/}
+            // {/*                />*/}
+            // {/*                Ik schrijf me in voor de nieuwsbrief*/}
+            // {/*            </span>*/}
+            //
+            // {/*            <button*/}
+            // {/*                type="submit"*/}
+            // {/*                id="submit_button"*/}
+            // {/*            >*/}
+            // {/*                Versturen*/}
+            // {/*            </button>*/}
+            // {/*        </fieldset>*/}
+            //
+            // {/*    </form>*/}
+            // {/*</wrapper>*/}
 
 
--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,22 +1,53 @@
 import React from "react";
 
 function MyForm({form_title, legend, label_field_one, input_type, input_type_two, input_id, input_id_two, label_field_two, legend_two, textarea_id, textarea_label, placeholder}) {
-return (
-    <wrapper>
+
+    const [state, setState] = React.useState({
+        nameValue: "",
+        ageValue: 0,
+        messageValue: ""
+    })
+
+    // let [nameValue, setNameValue] = React.useState('');
+    // let [ageValue, setAgeValue] = React.useState(18);
+    // let [messageValue, setMessageValue] = React.useState('');
+
+    function handleSubmit(e) {
+        console.log(`Naam: ${state.nameValue} Leeftijd: ${state.ageValue} Opmerking: ${state.messageValue}`);
+        e.preventDefault();
+
+    }
+
+    function handleChange(e) {
+        const value = e.target.value;
+        setState({...state,[e.target.name]:value});
+    }
+
+    return (
+    <div className="wrapper">
         <h1>{form_title}</h1>
 
-        <form id="review" >
+        <form id="review" onSubmit={handleSubmit}>
             <fieldset>
                 <legend> {legend} </legend>
 
                 <label htmlFor={input_id}>{label_field_one}</label>
                 <input
+                    name="nameValue"
                     type={input_type}
                     id={input_id}
+                    value={state.nameValue}
+                    onChange={handleChange}
                 />
 
                 <label htmlFor={input_id_two}>{label_field_two}</label>
-                <input type={input_type_two} id={input_id_two}/>
+                <input
+                    name="ageValue"
+                    type={input_type_two}
+                    id={input_id_two}
+                    value={state.ageValue}
+                    onChange={handleChange}
+                />
 
             </fieldset>
 
@@ -25,8 +56,11 @@ return (
 
                 <label htmlFor={textarea_id}>{textarea_label}</label>
                 <textarea
+                    name="messageValue"
                     id={textarea_id}
                     placeholder={placeholder}
+                    value={state.messageValue}
+                    onChange={handleChange}
                 />
 
                 <span>
@@ -46,7 +80,7 @@ return (
             </fieldset>
 
         </form>
-    </wrapper>
+    </div>
 
 )}
 
